6bdd3cbd858f7ad6cc1b3883cb0e4add8a1e5373
Emily Yao <emily2004.yao@gmail.com>
2023-11-08 08:45:23 -0600

    breaking script features into functions

---
+++ _file.py
@@ -0,0 +1,165 @@
+"""
+Deals with scraping and formatting information from yaml files.
+"""
+
+import os
+import yaml
+from collections import defaultdict
+
+XT = "/etc/sysconfig/network-scripts/ifcfg-"
+SKIP = {
+  "spaldingwcic0.chtc.wisc.edu.yaml", # some issue with printing out an ip addr...?
+  "path-ap2001.chtc.wisc.edu.yaml", # there's a '\t' which throws on line125, col11
+  "glidein-cm3000.chtc.wisc.edu.yaml" # has unknown char on line28, col55
+} 
+nodes_mp = {
+  "vxlan123": ["0601", "1200"],
+  "eth0": ["0600", "0601"],
+  "eth1": ["0600", "0601"],
+  "bond0": ["0601"],
+  "ib0": ["0602"],
+  "br0": ["0601"]
+}
+
+
+def get_yaml_file(filepath: str) -> dict:
+  """
+  Loads yaml file located at FILEPATH.
+
+  Parameters
+  ----------
+  filepath : str
+    where the file is located
+
+  Returns
+  -------
+  dict
+    dictionary representation of yaml file
+
+  Raises
+  ------
+  YAMLError
+    if an issue occurs while parsing the yaml file into a dictionary
+  """
+  with open(f'{filepath}', "r") as stream:
+    try: 
+      return yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+      exit(f'An error occured in: {filepath}\n{exc}')
+
+
+def get_subnets() -> dict:
+  """
+  Scrapes subnet masks from each file in the "sites" directory.
+
+  Returns
+  -------
+  dict
+    dictionary mapping each file in "sites" to corresponding subnet masks for each connection type 
+  """
+  subnets = defaultdict()
+
+  for file in os.listdir("./sites"):
+    data = get_yaml_file(f'./sites/{file}')
+    subnets[file] = defaultdict()
+  
+    for t in ["eth0", "bond0", "br0"]: # the other types don't show up or don't have notable info
+      mask = ""
+
+      try:
+        if (d := data["file"][f'{XT}{t}']["content"]):
+          if "0701" in d:
+            mask = d["0701"].keys()
+          elif "0700" in d and d["0700"]: # can sometimes be false
+            mask = d["0700"].keys()
+ 
+      except KeyError:
+        pass
+
+      if mask:
+        subnets[file][t] = list(mask)[0].split("=")[1]
+
+  return subnets
+
+
+def get_nodes() -> dict:
+  """
+  Scrapes primary and BMC NIC IP address information from each file in the "nodes" directory.
+
+  Returns
+  -------
+  dict
+    nested dictionary mapping file names to another dictionary, which stores primary/BMC IP 
+    addresses in string format
+  """
+  addrs = defaultdict()
+
+  for file in os.listdir("./nodes"):
+    if file in SKIP:
+      continue
+
+    data = get_yaml_file(f'./nodes/{file}')
+    f = file.split(".")[0]
+    addrs[f] = defaultdict()
+
+    if "bmc" in data.keys():
+      addrs[f]["bmc"] = get_bmc_addrs(data)
+    addrs[f]["primary"] = get_primary_addrs(data)
+  
+  return addrs
+
+
+def get_bmc_addrs(node: dict) -> list:
+  """
+  Gets IP addresses of BMC NICs from NODE.
+
+  Parameters
+  ----------
+
+  Returns
+  -------
+  """
+  if "default_gateway_ip" in node["bmc"]["lan"].keys():
+    return [
+      node["bmc"]["lan"]["ip_address"], 
+      node["bmc"]["lan"]["default_gateway_ip"]
+    ]
+  return [node["bmc"]["lan"]["ip_address"]]
+
+
+def get_primary_addrs(node: dict) -> list:
+  """
+  Gets IP addresses of primary NICs from NODE.
+
+  Parameters
+  ----------
+
+  Returns
+  -------
+  """
+  addrs = []
+
+  ### CENTOS7 ###
+  if "network" in node.keys():
+    if "default_gateway" in node["network"]:
+      addrs.append(node["network"]["default_gateway"])
+    if "bridge_static" in node["network"]:
+      addrs.append(node["network"]["bridge_static"]["br0"]["ipaddress"])
+
+  ### CENTOS8 ###    
+  elif "file" in node.keys():
+    for k, vs in nodes_mp.items():
+      for v in vs:
+        # using a try-except instead of nested ifs to check if the specific yaml field containing
+        # the IP address exists in the current file
+        try:
+          if (ip := node["file"][f'{XT}{k}']["content"][v]) != False:
+            if v == "1200" and k == "vxlan123":
+              addrs.append(list(ip.keys())[1].split("=")[1])
+            else:
+              addrs.append(list(ip.keys())[0].split("=")[1])
+
+        except KeyError:
+          pass
+  
+  return addrs
--- _sort.py
+++ _sort.py
@@ -1,19 +1,12 @@
-import os
-import yaml
-from collections import defaultdict
-import pprint
-
-# TODO: map each node to its "site" and then look for network/host addrs based on that
-
-XT = "/etc/sysconfig/network-scripts/ifcfg-"
-sites_opts = ["eth0", "bond0", "br0"]
-subnets = defaultdict()
-
-# TODO: how do i type enforce this like in swift
 """
-Returns tuple of (NETWORK, HOST) addresses after applying subnet mask
+Subnet mask calculations
 """
-def get_addresses_from_subnet_mask(addr, mask):
+
+
+def get_addresses_from_subnet_mask(addr: str, mask: str) -> tuple:
+  """
+  Returns tuple of (NETWORK, HOST) addresses after applying subnet mask
+  """
   _addr = list(map(int, addr.split(".")))
   _mask = list(map(int, mask.split(".")))
   pos = []
@@ -35,92 +28,3 @@ def get_addresses_from_subnet_mask(addr, mask):
     neg.append(".")
   
   return (''.join(pos[:-1]), ''.join(neg[:-1]))
-
-print(get_addresses_from_subnet_mask("192.168.123.132", "255.255.255.0"))
-
-
-for file in os.listdir("./sites"):
-  with open(f'./sites/{file}', "r") as stream:
-    try: 
-      data = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-      print(file)
-      print(f'!!! ERROR {exc} ERROR !!!')
-      exit(0)
-
-  subnets[file] = defaultdict()
-  for t in sites_opts:
-    try:
-      subnets[file][t] = list(data["file"][f'{XT}{t}']["content"]["0701"].keys())[0].split("=")[1]
-    except KeyError as err:
-      try:
-        if data["file"][f'{XT}{t}']["content"]["0700"]:
-          subnets[file][t] = list(data["file"][f'{XT}{t}']["content"]["0700"].keys())[0].split("=")[1]
-      except KeyError as err:
-        pass
-
-pprint.pprint(subnets)
-
-#####
-
-SKIP = { "spaldingwcic0.chtc.wisc.edu.yaml", "unl-svc-1.facility.path-cc.io.yaml",
-         "path-ap2001.chtc.wisc.edu.yaml", "glidein-cm3000.chtc.wisc.edu.yaml"}
-nodes_mp = {"vxlan123": ["0601", "1200"],
-            "eth0": ["0600", "0601"],
-            "eth1": ["0600", "0601"],
-            "bond0": ["0601"],
-            "ib0": ["0602"],
-            "br0": ["0601"]}
-
-for file in os.listdir("./nodes"):
-  if file in SKIP:
-    continue
-  
-  # checking if file is good to parse
-  with open(f'./nodes/{file}', "r") as stream:
-    try: 
-      data = yaml.safe_load(stream)
-      print(f'---{file.split(".")[0]}---')
-    except yaml.YAMLError as exc:
-      print(file)
-      print(f'!!! ERROR {exc} ERROR !!!')
-      exit(0)
-
-    #########
-    ## BMC ##
-    #########
-    # TODO: looks like it will always be same xxx.xxx.xxx.??? so this might be subnet issue
-    if "bmc" in data.keys():
-      print(f'BMC Networks:')
-      print(f'    {data["bmc"]["lan"]["ip_address"]}')
-      if "default_gateway_ip" in data["bmc"]["lan"].keys():
-        print(f'    {data["bmc"]["lan"]["default_gateway_ip"]}')
-
-    ##########
-    ## DATA ##
-    ##########
-    print(f'Data Networks:')
-
-    ### CENTOS7 ###
-    if "network" in data.keys():
-      if "default_gateway" in data["network"]:
-        print(f'    {data["network"]["default_gateway"]}')
-      if "bridge_static" in data["network"]:
-        print(f'    {data["network"]["bridge_static"]["br0"]["ipaddress"]}')
-      continue
-
-    ### CENTOS8 ###    
-    if "file" in data.keys():
-      for k, vs in nodes_mp.items():
-        for v in vs:
-          # using a try-except instead of nested ifs to check if the specific yaml field containing
-          # the IP address exists in the current file
-          try:
-            if (ip := data["file"][f'{XT}{k}']["content"][v]) != False:
-              if v == "1200" and k == "vxlan123":
-                print(f'    {list(ip.keys())[1].split("=")[1]}')
-              else:
-                print(f'    {list(ip.keys())[0].split("=")[1]}')
-
-          except KeyError as err:
-            pass
